7fe5f38781cf8a41d8c6d2daf0ed2931314ccac3
yialhifzi <66778154+yialhifzi@users.noreply.github.com>
2022-06-05 16:14:03 +0300

    Create function_basic_2.py

---
+++ _python/python_test/function_basic_2.py
@@ -0,0 +1,43 @@
+# !! Uncomment #Number for solution !!
+
+# #1
+# def countdown(x):
+#     arr=[]
+#     for x in range(x,-1,-1):
+#         arr.append(x)
+#     print(arr)
+# countdown(5)
+
+# #2
+# def print_and_return(arr):
+#     print(arr[0])
+#     return(arr[1])
+# print(print_and_return([1,2]))
+
+# #3
+# def first_plus_length(arr):
+#     print(arr[0]+len(arr))
+# first_plus_length([1,2,3,5])
+
+# #4
+# def values_greater_than_second(lst):
+#     newLst=[]
+#     v=True
+#     if len(lst)<3:
+#         v=False
+#         print(v)
+#     elif v==True:
+#         for x in range(0, len(lst)):
+#             if lst[x]>lst[1]:
+#                 newLst.append(lst[x])
+#         print(len(newLst))
+#         print(newLst)
+# values_greater_than_second([1,2,5,6,4,3,0])
+
+# #5
+# def length_and_value(x,y):
+#     newLst=[]
+#     for x in range(0,x):
+#         newLst.append(y)
+#     print(newLst)
+# length_and_value(4,1)
