043c1a1daca34d3e969d9c39990e0dc6c7c59278
yialhifzi <66778154+yialhifzi@users.noreply.github.com>
2022-06-08 11:58:16 +0300

    Update zoo.py

--- _python/python_test/zoo.py
+++ _python/python_test/zoo.py
@@ -1,17 +1,17 @@
 class Zoo:
     def __init__(self, zname):
-        self.animalist = []
+        # self.animalist = []
         self.zname = zname
-    def add_tiger(self, name):
-        self.animalist.append( Tiger(name) )
-    def add_lion(self, name):
-        self.animalist.append( Lion(name) )
-    def add_rabbit(self, name):
-        self.animalist.append( Rabbit(name) )
-    def print_all_info(self):
-        print("-"*30, self.zname, "-"*30)
-        for animal in self.animalist:
-            animal.display_info()
+    # def add_tiger(self, name):
+    #     self.animalist.append( Tiger(name) )
+    # def add_lion(self, name):
+    #     self.animalist.append( Lion(name) )
+    # def add_rabbit(self, name):
+    #     self.animalist.append( Rabbit(name) )
+    # def print_all_info(self):
+    #     print("-"*30, self.zname, "-"*30)
+    #     for animal in self.animalist:
+    #         animal.display_info()
 
 class Animal:
     def __init__(self, name, age, health, happiness):
@@ -26,12 +26,12 @@ class Animal:
     def display_info(self):
         print(f"Name: {self.name} | Age: {self.age} | Health: {self.health} | Happiness: {self.happiness}")
 
-class Tiger(Animal):
-    def __init__(self, name, age=8, health=120, happiness=100):
-        super().__init__(name, age, health, happiness)
-
 class Lion(Animal):
     def __init__(self, name, age=10, health=100, happiness=80):
+        super().__init__(name, age, health, happiness)
+
+class Tiger(Animal):
+    def __init__(self, name, age=8, health=120, happiness=100):
         super().__init__(name, age, health, happiness)
 
 class Rabbit(Animal):
@@ -40,8 +40,18 @@ class Rabbit(Animal):
 
 
 
+Simba = Lion("Simba")
+ShereKhan = Tiger("Shere Khan")
+Arnab = Rabbit("Arnab")
+
 zoo1 = Zoo("Yazeed's Zoo")
-zoo1.add_lion("Simba")
-zoo1.add_tiger("Shere Khan")
-zoo1.add_rabbit("Arnab")
-zoo1.print_all_info()
+# zoo1.add_lion("Simba")
+# zoo1.add_tiger("Shere Khan")
+# zoo1.add_rabbit("Arnab")
+
+print("-"*30, zoo1.zname, "-"*30)
+Simba.feed().feed().display_info()
+ShereKhan.feed().feed().display_info()
+Arnab.feed().feed().display_info()
+
+# zoo1.print_all_info()
