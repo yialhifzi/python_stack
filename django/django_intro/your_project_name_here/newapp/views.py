dd25e2dbe96cfe9ca47c5560f82429340d7e17d2
yialhifzi <66778154+yialhifzi@users.noreply.github.com>
2022-06-12 09:52:16 +0300

    Add files via upload

---
+++ django/django_intro/your_project_name_here/newapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse, redirect
+def root(request):
+    return redirect("/blogs")
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+def create(request):
+    return redirect("/")
+def show(request, number):
+    return HttpResponse("placeholder to display blog number: {number}")
+def edit(request, number):
+    return HttpResponse("placeholder to edit blog {number}")
+def destroy(request, number):
+    return redirect("/blogs")
